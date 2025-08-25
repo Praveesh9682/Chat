d0701cc74a970f76c70e4388cadb0bc1a801b3c7
Praveesh9682 <78218402+Praveesh9682@users.noreply.github.com>
2025-08-25 08:24:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+// If you have any other static files like CSS/JS/images in the same folder, serve them from here
+app.use(express.static(__dirname));
 
 // Socket.io connection event
 io.on('connection', (socket) => {
@@ -31,17 +32,13 @@ io.on('connection', (socket) => {
     });
 });
 
-// Serve the index.html file on the root route
+// Serve the index.html file from the same folder
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// // Start the server on port 3000
-// server.listen(3000, '0.0.0.0', () => {
-//     console.log('Server is running on http://localhost:3000');
-// });
+// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
